1b5d207c1f4e578f70d71834a2985576631809f0
reynolds <reynolds>
2009-11-24 17:04:39 +0000

    Dance with the one that brung ye.

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -33,9 +33,9 @@ if [ ! -x ./configure ]; then
 	exit 1
 fi
 echo "Running configure with preferred arguments"
-export	CC='ccache gcc -std=gnu99'
+export	CC='ccache gcc -std=gnu99 -march=native'
 export	CFLAGS='-pipe -Os'
-export	CXX='ccache g++'
+export	CXX='ccache g++ -march=native'
 export	CXXFLAGS='-pipe -Os'
 ./configure								\
 	$@
